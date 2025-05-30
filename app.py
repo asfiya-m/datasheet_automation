ccf1aa3129259c26953a62def49ff793c6b94c87
asfiya-m <asfiya@marianaminerals.com>
2025-05-30 16:33:48 -0700

    Update app.py to support dynamic download with timestamped filename

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ import streamlit as st
 from automation_test1 import generate_master_datasheet
 
 st.set_page_config(page_title="Master Equipment Sheet Generator", layout="centered")
-st.title("📊 Master Equipment Sheet Generator")
+st.title("📊 Master Equipment Data Sheet Generator")
 
 uploaded_file = st.file_uploader("Upload your Datasheets Excel (.xlsm)", type=["xlsm"])
 
 if uploaded_file:
     if st.button("Generate Master DataSheet"):
         with st.spinner("Processing..."):
-            output_path = generate_master_datasheet(uploaded_file)
+            output,filename = generate_master_datasheet(uploaded_file)
         st.success("Done! ✅")
-        with open(output_path, "rb") as f:
-            st.download_button("📥 Download Master Sheet", f, file_name=output_path.name)
+        #with open(output_path, "rb") as f:
+        st.download_button("📥 Download Master DataSheet", output, file_name=filename)
 
